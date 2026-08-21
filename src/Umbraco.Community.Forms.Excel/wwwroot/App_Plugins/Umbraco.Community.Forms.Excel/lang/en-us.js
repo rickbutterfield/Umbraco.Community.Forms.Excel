b78031730f7d161b19f357b7f8d6c588810a8f15
Rick Butterfield <rcb@umbraco.dk>
2026-08-21 08:44:23 +0100

    Add backoffice translations for the Excel export types
    The export type list looked up formProviderExportTypes_{alias} and
    formProviderExportTypes_{alias}Description for both export types,
    but no translation was ever shipped for them. Adds an en-us
    localization extension via a static App_Plugins manifest, which
    required switching the project to Microsoft.NET.Sdk.Razor so
    wwwroot content packs into the nupkg as static web assets.

---
+++ src/Umbraco.Community.Forms.Excel/wwwroot/App_Plugins/Umbraco.Community.Forms.Excel/lang/en-us.js
@@ -0,0 +1,6 @@
+export default {
+  formProviderExportTypes_excelFileSubmittedValues: 'Excel file (submitted values)',
+  formProviderExportTypes_excelFileSubmittedValuesDescription: 'Exports all submitted values to an Excel file.',
+  formProviderExportTypes_excelFileDisplayValues: 'Excel file (display values)',
+  formProviderExportTypes_excelFileDisplayValuesDescription: 'Exports all display values to an Excel file. Captions are used for prevalue data where available.',
+};
